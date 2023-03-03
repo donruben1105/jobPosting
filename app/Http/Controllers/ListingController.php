2b5714643f0ad05e7b7a6351cfa6d70bc6d41a63
donruben1105 <donruben1105@gmail.com>
2023-03-04 05:17:39 +0800

    initial commit

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    //Show all listings
+    public function index() {
+        return view('listings.index', [
+            'listings' => Listing::latest()->filter(request(['tag','search']))->paginate(4)
+        ]);
+    }
+
+    //show single listing
+    public function show(Listing $listing) {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    //Show Create Form
+    public function create() {
+        return view('listings.create');
+    }
+
+    //Store Listing Data
+    public function store(Request $request) {        
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required',
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required','email'],
+            'tags' => 'required',
+            'file' => 'required',
+            'description' => 'required',
+        ]);
+    
+        if($request->hasFile('file')) {
+            $formFields['file'] = $request->file('file')->store('files', 'public');
+        }
+    
+        $formFields['user_id'] = auth()->id();
+    
+        Listing::create($formFields);
+    
+        // Get the URL for the stored file
+        $fileUrl = asset('storage/' . $formFields['file']);
+    
+        return redirect('/')->with('message', 'Listing created successfully!')->with('fileUrl', $fileUrl);
+    }
+    
+
+    //Show Edit Form
+    public function edit(Listing $listing) {       
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    //Update Listing Data
+    public function update(Request $request, Listing $listing) {   
+        //Make sure logged in user is owner
+        if($listing->user_id !=auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+        
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required', 
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required','email'],
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing updated successfully!');
+    }
+    
+    //Delete Listing
+    public function destroy(Listing $listing) {
+        //Make sure logged in user is owner
+        if($listing->user_id !=auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted successfully!');        
+    }
+
+    //manage Listings
+    public function manage() {
+        return view('listings.manage', ['listings' => auth()->user()->listings()->get()]);
+    }
+}
